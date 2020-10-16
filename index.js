59446be1b7444dd323b47602d82608c949ae8717
namhai98 <62587065+namhai98@users.noreply.github.com>
2020-10-16 14:47:45 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+function Mendchilgee(){//function component, props-uгүй
+    return<h1>Narka inda house yeah</h1>;
+    }
+    function Mendchilgee123(props){//function component, props-uгүй
+        return<h1>{props.ner}</h1>;
+            }
+
+    function Mendchilgee456(props){//function component, props-uгүй
+                return(
+                    <div>
+                        <h1>{props.ner}</h1>
+                        <p>{props.nas}</p>
+                    </div>
+                )
+           }
+    function EtsegComp(props){
+        return(
+            <div>
+                <Mendchilgee/>
+                <Mendchilgee123 ner={props.ner}/>
+                <Mendchilgee456 ner={props.ner} nas={props.nas}/>
+                <table>
+                    <tr><td> {props.angi[0]}</td></tr>
+                    <tr><td> {props.angi[1]}</td></tr>
+                    <tr><td> {props.angi[2]}</td></tr>
+                    
+                </table>
+            </div>
+        )
+    }
+              ReactDOM.render(
+                //  <Mendchilgee/>,
+               // <Mendchilgee123 ner="bat"/>,
+               // <Mendchilgee456 ner="bat" nas='55'/>,
+               <EtsegComp ner="james" nas="23" angi={['ПХ','МС','НББМС']}/>,
+                document.getElementById('root')
+              );
+            
